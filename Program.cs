f9f490d699eb9020d62d33ae4a727898a0924e5a
Sanjay Yaduwanshi <sanjay.yaduwanshi@mindpathtech.com>
2023-08-22 11:04:28 +0530

    Initial code

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+interface ISanju
+{
+    void Show();
+}
+interface IAj:ISanju
+{ void Show1(); }
+class B: IAj
+{
+   public void Show()
+    {
+        Console.WriteLine("interface inter1");
+    }
+    public void Show1() 
+    {
+        Console.WriteLine("interface inter2");
+    }
+
+}
+class A
+{
+    public static void Main()
+    {
+        B b = new B();
+        b.Show();
+        b.Show1();
+    }
+} 
